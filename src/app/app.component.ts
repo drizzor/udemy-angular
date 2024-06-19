fd90cfeff808f3fb0394b8e73582bebe67a7e3d5
Kevin M <k.mary@live.be>
2024-06-19 15:32:16 +0200

    P2-34 Working w Outputs and emitting date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { TasksComponent } from './tasks/tasks.component';
 import { DUMMY_USERS } from './dummy-users';
 
 @Component({
@@ -8,8 +9,19 @@ import { DUMMY_USERS } from './dummy-users';
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUserId = 'u1';
+
+  get selectedUser() {
+    return this.users.find(user => user.id === this.selectedUserId)!;
+  }
+
+  OnSelectUser(id: string) {
+    // if (DUMMY_USERS.find(user => user.id === id))
+    //   this.selectedUserId = DUMMY_USERS.find(user => user.id === id)!.name;
+    this.selectedUserId = id;
+  }
 }
