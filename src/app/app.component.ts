397b4343d34be1987a1fb879bf5dd030a82e6d2a
Kevin M <k.mary@live.be>
2024-06-20 09:18:58 +0200

    P2-43 Legacy Angular Using ngFor et ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { TasksComponent } from './tasks/tasks.component';
@@ -9,7 +11,7 @@ import { DUMMY_USERS } from './dummy-users';
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent, UserComponent, TasksComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
 })
 export class AppComponent {
   users = DUMMY_USERS;
